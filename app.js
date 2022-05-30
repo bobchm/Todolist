f09ee6cea2536e323672e6b35b614a9781d0d25d
Bob Cunningham <bobchm@gmail.com>
2022-05-30 11:07:05 -0400

    Change todolist to access Mongo DB on Atlas on the cloud

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://bobchm:monkeyhead@cluster0.4c99z.mongodb.net/todolistDB");
 
 const topLevelList = "Today";
 var activeList = topLevelList;
